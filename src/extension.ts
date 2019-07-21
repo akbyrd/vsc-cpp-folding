33f7c9641f687c6856d01ab5daeb56c5f20b85d2
Adam Byrd <adam1.byrd@gmail.com>
2019-07-21 13:13:24 -0700

    Add debug config option

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,73 @@
 import * as vscode from 'vscode';
 
-let log: vscode.OutputChannel;
+let addon: any;
+let logWindow: vscode.OutputChannel;
+let config: vscode.workspace.WorkspaceConfiguration;
+let debug: boolean;
+
+function log(...strs: any[]) {
+	for (let s of strs) {
+		logWindow.append(s.toString());
+	}
+	logWindow.appendLine('');
+}
+
+function debugLog(...strs: any[]) {
+	if (debug) {
+		for (let s of strs) {
+			logWindow.append(s.toString());
+		}
+		logWindow.appendLine('');
+	}
+}
+
+function onDidChangeConfiguration(event: vscode.Event<vscode.ConfigurationChangeEvent>) {
+	log('onDidChangeConfiguration');
+	config = vscode.workspace.getConfiguration();
+	debug = config.get<boolean>('cppfold.debug');
+	log('debug: ', debug);
+}
+
+function provideFoldingRanges(
+	document: vscode.TextDocument,
+	context: vscode.FoldingContext,
+	token: vscode.CancellationToken
+) {
+	let result = addon.ParseFile(document.fileName, document.getText());
+
+	// Errors
+	let errors: string[] = result.errors;
+	for (let error of errors) {
+		log('Error: ' + error);
+	}
+	if (debug && errors.length > 0) {
+		logWindow.show(true);
+	}
+
+	// Ranges
+	let ranges: vscode.FoldingRange[] = result.ranges;
+	debugLog(document.fileName + ' - ' + ranges.length + ' Ranges');
+	return ranges;
+}
 
 export function activate(context: vscode.ExtensionContext) {
-	// TODO: Dev only
-	let debug: boolean = false;
+	logWindow = vscode.window.createOutputChannel('C++ Code Folding');
+	log('activate');
 
-	log = vscode.window.createOutputChannel('C++ Code Folding');
-	log.show(debug);
+	onDidChangeConfiguration(null);
+	if (debug) {
+		logWindow.show(true);
+	}
 
 	// TODO: Do we need to dispose of anything?
 	//context.subscriptions.push(disposable);
 
-	let addon = require('bindings')('binding');
+	debugLog('Loading bindings...');
+	addon = require('bindings')('binding');
+	debugLog('Loaded bindings');
 
-	vscode.languages.registerFoldingRangeProvider('cpp', {
-		provideFoldingRanges(
-			document: vscode.TextDocument,
-			context: vscode.FoldingContext,
-			token: vscode.CancellationToken)
-		{
-			let result = addon.ParseFile(document.fileName, document.getText());
-
-			// Errors
-			let errors: string[] = result.errors;
-			for (let error of errors) {
-				log.appendLine('Error: ' + error);
-			}
-			//log.show(errors.length > 0);
-
-			// Ranges
-			let ranges: vscode.FoldingRange[] = result.ranges;
-			log.appendLine(document.fileName + ' - ' + ranges.length + ' Ranges');
-			return ranges;
-		}
-	});
+	vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration);
+	vscode.languages.registerFoldingRangeProvider('cpp', { provideFoldingRanges });
 }
 
-export function deactivate() {}
+//export function deactivate() {}
